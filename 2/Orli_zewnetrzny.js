b546cb33b24b4e85a2da473dd252cf70bfd41721
Halopolicj <xxcien.smiercixx@gmail.com>
2026-09-10 10:09:54 +0200

    Add files via upload

---
+++ 2/Orli_zewnetrzny.js
@@ -0,0 +1,2 @@
+document.write("ostatnia modyfikacja strony".fontcolor("#808080").strike().fontsize(6)+"<br>");
+document.write(document.lastModified.fontcolor("#00FFFF").bold().fontsize(6)+"<br>");
